85076f12149bcf71438a603c25fed34f551c64b2
Ankit070mishra <ankit070mishra@gmail.com>
2022-08-29 22:11:08 +0530

    Should update the UI tomorrow

--- Support/modalScript.js
+++ Support/modalScript.js
@@ -7,13 +7,20 @@ const modalSubmitBtnClick = (option) => {
         xhttp.onload = function() {
             console.log(xhttp.responseText);
         }
-        xhttp.open("GET", "Support/trial.php?amount="+amount+"&reason="+reason+"&option=1", true);
+        xhttp.open("GET", "Support/modalDataHandler.php?amount="+amount+"&reason="+reason+"&option=0", true);
         xhttp.send();
 
     }
     else if(option == 2)
     {
-        console.log("Option 2 Clicked")
+        var amount = parseInt(document.getElementById('expensedamount').value);
+        var reason = document.getElementById('expensedreason').value;
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            console.log(xhttp.responseText);
+        }
+        xhttp.open("GET", "Support/modalDataHandler.php?amount="+amount+"&reason="+reason+"&option=1", true);
+        xhttp.send();
     }
     else{
         console.log("Invalid option")
